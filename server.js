806635da26649e62fc2ff03ee90ee62e22ded3ed
MohammedMeraj <mdmomin7517@gmail.com>
2025-11-24 21:33:21 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+const os = require('os');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return 'localhost';
+}
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  }).listen(port, hostname, (err) => {
+    if (err) throw err;
+    const localIP = getLocalIP();
+    console.log(`\n✨ Ready on:\n`);
+    console.log(`   Local:    http://localhost:${port}`);
+    console.log(`   Network:  http://${localIP}:${port}\n`);
+  });
+});
